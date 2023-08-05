6070e60e7089f0f58d7ea203e8b09b31b2ad6901
Jihwan Kim <jihwankim@Jihwanui-MacBookPro.local>
2023-08-05 22:02:08 +0900

    Feature: divide each crds

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-kubectl apply -f confluent-platform.yaml
+kubectl apply -f confluent-kafka-zookeeper.yaml
 kubectl apply -f confluent-ksqldb.yaml
 kubectl apply -f confluent-schemaregistry.yaml
-kubectl apply -f confluent-restapi.yaml
 kubectl apply -f confluent-connect.yaml
+kubectl apply -f confluent-restapi.yaml
+kubectl apply -f confluent-controlcenter.yaml
+kubectl apply -f confluent-connector.yaml
